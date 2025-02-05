d1dfcc29e74df4c8fa0a36412222ee2b5fe3d451
Andrew Hrechyn <146077376+AndrewHrechyn@users.noreply.github.com>
2025-02-05 13:06:31 +0200

    Tasks solved

---
+++ LeetCode/C++/1929_Concatenation_of_Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> res = nums;
+
+        for (auto n: nums){
+            res.push_back(n);
+        }
+
+        return res;
+    }
+};
